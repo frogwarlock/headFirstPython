11ddbd611aa579da9ada2996d86d30d9b86c23b1
isalu <isabellalucena15@gmail.com>
2025-12-27 18:40:09 -0300

    feat: implementa funções para leitura de dados de natação e geração de gráficos em HTML
    - função para gerar gráficos de barras em HTML
    - ajuste de alguns returns e variáveis para utilização de f-string ao invés de concatenação
    - utilização de bibliotecas padrão do Python
    - Capítulo 5 do livro Use a Cabeça Python

--- sistema_monitoramento_tempos_natacao/capitulo_4/swimclub.py
+++ sistema_monitoramento_tempos_natacao/capitulo_4/swimclub.py
@@ -1,5 +1,7 @@
 import statistics
+import hfpy_utils
 
+CHARTS = "../charts/"
 FOLDER = '../swimdata/'
 
 def read_swim_data(filename):
@@ -26,9 +28,45 @@ def read_swim_data(filename):
             seconds, centis = time.split('.')
         converts.append((int(minutes) * 60 * 100) + (int(seconds) * 100) + int(centis))
     average = statistics.mean(converts)
-    mins_secs, centis = str(round(average/100,2)).split('.')
+    mins_secs, centis = f"{(average/100):.2f}".split('.')
     mins_secs = int(mins_secs)
     minutes = mins_secs // 60
     seconds = mins_secs - minutes * 60
-    average = str(minutes) + ':' + str(seconds) + '.' + str(centis)
-    return swimmer, age, distance, stroke, times, average #retorna uma tupla com os dados do nadador
+    average = f"{minutes}:{seconds:0>2}.{centis}"
+    return swimmer, age, distance, stroke, times, average, converts #retorna uma tupla com os dados do nadador
+
+def produce_bar_chart(fn):
+    
+    swimmer, age, distance, stroke, times, average, converts = read_swim_data(fn)
+    from_max = max(converts)
+    times.reverse()
+    converts.reverse()
+    title = f"{swimmer} (Under {age} {distance} {stroke})"
+    header = f"""<!DOCTYPE html>
+                    <html>
+                        <head>
+                            <title>{title}</title>
+                        </head>
+                        <body>
+                            <h3>{title}</h3>
+    """
+    body = ""
+    for index, time in enumerate(times):
+        bar_width = hfpy_utils.convert2range(converts[index], 0, from_max, 0, 350)
+        body = body + f"""
+                            <svg height="30" width="400">
+                                <rect height="30" width="{bar_width}" style="fill:rgb(0,0,255);" />
+                            </svg> {time} <br/>
+        """
+    footer = f"""
+                            <p>Average time: {average} </p>
+                        </body>
+                    </html>
+    """
+    page = header + body + footer
+    save_to = f"{CHARTS}{fn.removesuffix('.txt')}.html"
+    with open(save_to, "w") as sf:
+        print(page, file=sf)
+        
+    return save_to
+    
